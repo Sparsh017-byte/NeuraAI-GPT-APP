1be9c2cc24087f9b88e7ee89da7b52c197f14553
Sparsh0177 <sparshgumasta811@gmail.com>
2025-10-06 13:28:48 +0530

    Update geminiApi.js

--- gemini-clone/src/api/geminiApi.js
+++ gemini-clone/src/api/geminiApi.js
@@ -2,6 +2,7 @@ const API_URL = import.meta.env.REACT_APP_API_URL|| "http://localhost:5000";
 
 
 export async function generateText(prompt) {
+  
   try {
     const res = await fetch(`${API_URL}/generate`, {
       method: "POST",
